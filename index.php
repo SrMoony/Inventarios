9178c4907fa9d33b429540308a6ab8d8cad6ad56
David Herrera <david_gilherrera@hotmail.com>
2022-11-20 20:23:38 -0600

    Primer commit, se inicia el proyecto

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="shortcut icon" href="./imagenes/icono.ico">
+</head>
+
+<body>
+    <header class="header">
+
+    </header>
+
+    <main class="main">
+        esto deberia funcinar
+    </main>
+
+    <footer class="footer">
+
+    </footer>
+</body>
+
+</html>
